f83dab1a5344af3c7e9a019a43ec76b8a708a161
petra_bodman <petra_bodman@hotmail.com>
2018-03-15 11:30:52 +0100

    rspec: 4 examples, 0 failures

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,17 @@
 def fizz_buzz(number)
 
+#if number is divisible by 3 and by 5, show fizzbuzz
 if number % 3 == 0 && number % 5 == 0
-    puts 'fizzbuzz'
+    'fizzbuzz'
+
+#if number is divisible by 3, show fizz
   elsif number % 3 == 0
     'fizz'
+
+#if number is divisible by 5, show buzz
   elsif number % 5 == 0
     'buzz'
+#else if number is not divisible by 3 or/and 5, show number
   else
     number
   end
